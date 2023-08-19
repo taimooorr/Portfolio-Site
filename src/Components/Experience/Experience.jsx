f819558d66f9d10eeadb90a66821cf942a788dee
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-08-19 12:18:19 +0500

    initial commit

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Experience.css'
+export default function Experience() {
+  return (
+    <div>Experience</div>
+  )
+}
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Nav.css'
+export default function Nav() {
+  return (
+    <div>Nav</div>
+  )
+}
