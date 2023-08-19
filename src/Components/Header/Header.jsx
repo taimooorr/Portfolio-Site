5292930a5a853d84af2c0495977e529dae4b0350
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-08-19 16:38:01 +0500

    added nav bar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import './Header.css'
+import CTA from "./CTA";
+
+/* Assets */
+import ME from "../../assets/me.png";
+import HeaderSocial from "./HeaderSocial";
+
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+		<header>
+			<div className="container header__container">
+				<h5>Hello I'm</h5>
+				<h1>Taimoor Khan</h1>
+				<h5>Frontend Developer</h5>
+				<CTA />
+				<HeaderSocial />
+				<div className="me">
+					<img src={ME} alt="Taimoor Khan" />
+				</div>
+				<a href="contact" className="scroll__down">
+					Scroll Down
+				</a>
+			</div>
+		</header>
+  );
 }
